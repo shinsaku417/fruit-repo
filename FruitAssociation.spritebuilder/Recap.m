e8cec2ca9a0aa5a801d85ca0a3b425f20ffc7634
Shin Uesugi <shinsaku417@gmail.com>
2014-08-19 15:37:47 -0700

    recap bounce

--- FruitAssociation.spritebuilder/Recap.m
+++ FruitAssociation.spritebuilder/Recap.m
@@ -9,8 +9,15 @@
 #import "Recap.h"
 
 @implementation Recap {
+    CCLabelTTF *_gameOver;
+    
+    CCLabelTTF *_scoreMessage;
     CCLabelTTF *_scoreLabel;
+    
+    CCLabelTTF *_highscoreMessage;
     CCLabelTTF *_highscoreLabel;
+    
+    CCButton *_againButton;
 }
 
 - (void)onEnter {
@@ -19,6 +26,16 @@
     NSUserDefaults *gameState = [NSUserDefaults standardUserDefaults];
     _scoreLabel.string = [NSString stringWithFormat:@"%i", [gameState integerForKey:@"score"]];
     _highscoreLabel.string = [NSString stringWithFormat:@"%i", [gameState integerForKey:@"highscore"]];
+    
+    [self bounce:_gameOver];
+    [self bounce:_scoreMessage];
+    [self bounce:_scoreLabel];
+    [self bounce:_highscoreMessage];
+    [self bounce:_highscoreLabel];
+    
+    CCActionMoveTo *moveTo = [CCActionMoveTo actionWithDuration:1.0f position:ccp(0.5, _againButton.position.y)];
+    CCActionEaseBounceOut *bounceOut = [CCActionEaseBounceOut actionWithAction:moveTo];
+    [_againButton runAction:bounceOut];
 }
 
 - (void)again {
@@ -26,5 +43,12 @@
     CCTransition *transition = [CCTransition transitionFadeWithDuration:0.8f];
     [[CCDirector sharedDirector] presentScene:gameplayScene withTransition:transition];
 }
+
+- (void)bounce:(CCSprite *)sprite {
+    CCActionMoveTo *moveTo = [CCActionMoveTo actionWithDuration:1.0f position:ccp(0.5, sprite.position.y)];
+    CCActionEaseBounceOut *bounceOut = [CCActionEaseBounceOut actionWithAction:moveTo];
+    [sprite runAction:bounceOut];
+}
+
 
 @end

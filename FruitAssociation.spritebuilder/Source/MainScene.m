75a59ee66b6a1a2cdd9f3123e16476ef3b418add
Shin Uesugi <shinsaku417@gmail.com>
2014-08-19 14:53:52 -0700

    mainscene animation

--- FruitAssociation.spritebuilder/Source/MainScene.m
+++ FruitAssociation.spritebuilder/Source/MainScene.m
@@ -8,7 +8,48 @@
 
 #import "MainScene.h"
 
-@implementation MainScene
+@implementation MainScene {
+    CCSprite *_orange;
+    CCSprite *_apple;
+    CCSprite *_cherry;
+    CCSprite *_lemon;
+    CCSprite *_banana;
+    CCSprite *_grapes;
+    
+    CCLabelTTF *_title;
+    CCButton *_playButton;
+}
+
+- (void)onEnter {
+    [super onEnter];
+    
+    [self bounceSpriteUp:_orange];
+    [self bounceSpriteUp:_apple];
+    [self bounceSpriteUp:_cherry];
+    [self bounceSpriteDown:_lemon];
+    [self bounceSpriteDown:_banana];
+    [self bounceSpriteDown:_grapes];
+    
+    CCActionMoveTo *moveTo = [CCActionMoveTo actionWithDuration:1.5f position:ccp(0.5, _title.position.y)];
+    CCActionEaseBounceOut *bounceOut = [CCActionEaseBounceOut actionWithAction:moveTo];
+    [_title runAction:bounceOut];
+    
+    CCActionMoveTo *moveButton = [CCActionMoveTo actionWithDuration:1.5f position:ccp(0.5, _playButton.position.y)];
+    CCActionEaseBounceOut *bounceButton = [CCActionEaseBounceOut actionWithAction:moveButton];
+    [_playButton runAction:bounceButton];
+}
+
+- (void)bounceSpriteUp:(CCSprite *)sprite {
+    CCActionMoveTo *moveTo = [CCActionMoveTo actionWithDuration:1.0f position:ccp(sprite.position.x,sprite.position.y + 0.1)];
+    CCActionEaseBounceOut *bounceOut = [CCActionEaseBounceOut actionWithAction:moveTo];
+    [sprite runAction:bounceOut];
+}
+
+- (void)bounceSpriteDown:(CCSprite *)sprite {
+    CCActionMoveTo *moveTo = [CCActionMoveTo actionWithDuration:1.0f position:ccp(sprite.position.x,sprite.position.y - 0.1)];
+    CCActionEaseBounceOut *bounceOut = [CCActionEaseBounceOut actionWithAction:moveTo];
+    [sprite runAction:bounceOut];
+}
 
 - (void)play {
     CCScene *gameplayScene = [CCBReader loadAsScene:@"GamePlay"];
